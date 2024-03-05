1675ab3f26acb5b1f766ebdd6580657a534ae229
Arteom0 <142208261+Arteom0@users.noreply.github.com>
2024-03-05 15:48:35 +0300

    Add files via upload

---
+++ Diploma — копия/js/main.js
@@ -0,0 +1,29 @@
+(function () {
+
+    // слайдер
+
+    const swiper = new Swiper('.reviews__slider', {
+
+        pagination: {
+            el: '.reviews__pagination',
+
+        },
+
+        navigation: {
+            nextEl: '.reviews__next',
+            prevEl: '.reviews__prev',
+        },
+
+    });
+
+
+
+})()
+
+
+
+
+
+
+
+
